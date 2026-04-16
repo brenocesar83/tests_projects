f821fd230a952f396d0796004526c7e9a3172b75
Breno Cesar <brenocesar@gmail.com>
2026-04-15 22:06:00 -0300

    specs update

--- orange_hrm_ts/tests/specs/employee.spec.ts
+++ orange_hrm_ts/tests/specs/employee.spec.ts
@@ -5,13 +5,11 @@ import * as employeeSteps from '../steps/employee.steps';
 test('Cadastrar funcionario com sucesso', async ({ page }) => {
   await loginSteps.acessarLogin(page);
   await loginSteps.loginValido(page);
-
   await employeeSteps.criarFuncionario(page);
 });
 
 test('Validar campos obrigatorios ao criar funcionario', async ({ page }) => {
   await loginSteps.acessarLogin(page);
   await loginSteps.loginValido(page);
-
   await employeeSteps.validarCamposObrigatorios(page);
 });
